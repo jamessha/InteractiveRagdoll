5d11feec85a8e861295eccafd0d0154e1c27ad3b
SL <saasbook@saasbook.(none)>
2013-11-27 17:58:24 -0800

    updated ParticleSystem

--- particlesystem.cpp
+++ particlesystem.cpp
@@ -5,6 +5,51 @@ class Particle {
         Vector3d oldPos;
         Vector3d curPos;
         Vector3d acc;
+    public:
+        void constraints (Vector3d ll, Vector3d up, Particle* particles) {
+            double x = curPos[0];
+            double y = curPos[1];
+            double z = curPos[2];
+
+            double minx = ll[0];
+            double maxx = up[0];
+            double miny = ll[1];
+            double maxy = up[1];
+            double minz = ll[2];
+            double maxz = up[2];
+            if (x < minx) {
+                curPos[0] = (minx - curPos[0]) + minx;
+                oldPos[0] = minx - (oldPos[0] - minx);
+            }
+            if (x > maxx) {
+                curPos[0] = maxx - (curPos[0] - maxx);
+                oldPos[0] = (maxx - oldPos[0]) + maxx;           
+            }
+            if (y < miny) {
+                curPos[1] = (miny - curPos[1]) + miny;
+                oldPos[1] = miny - (oldPos[1] - miny);
+            }
+            if (y > maxy) {
+               curPos[1] = maxy - (curPos[1] - maxy);
+                oldPos[1] = (maxy - oldPos[1]) + maxy;            
+            }
+            if (z < minz) {
+                curPos[2] = (minz - curPos[2]) + minz;
+                oldPos[2] = minz - (oldPos[2] - minz);
+            }
+            if (z > maxz) {
+               curPos[2] = maxz - (curPos[2] - maxz);
+                oldPos[2] = (maxz - oldPos[2]) + maxz;             
+            }
+        }
+};
+
+class Sphere : public Particle {
+    public:
+        double radius;
+    public:
+        void constraints (Vector3d ll, Vector3d up, Particle* particles) {
+        }
 };
 
 class ParticleSystem {
@@ -57,32 +102,7 @@ void ParticleSystem::TimeStep() {
 void ParticleSystem::SatisfyConstraints() {
     for (int i = 0; i < num_particles; i++) {
         Particle part = particles[i];
-        double x = part.curPos[0];
-        double y = part.curPos[1];
-        double z = part.curPos[2];
-        if (x < 0) {
-            particles[i].curPos[0] = -particles[i].curPos[0];
-            particles[i].oldPos[0] = -particles[i].oldPos[0];
-        }
-        if (x > 10) {
-            particles[i].curPos[0] = 10 - (particles[i].curPos[0] - 10);
-            particles[i].oldPos[0] = (10 - particles[i].oldPos[0]) + 10;           
-        }
-        if (y < 0) {
-            particles[i].curPos[1] = -particles[i].curPos[1];
-            particles[i].oldPos[1] = -particles[i].oldPos[1];
-        }
-        if (y > 10) {
-           particles[i].curPos[1] = 10 - (particles[i].curPos[1] - 10);
-            particles[i].oldPos[1] = (10 - particles[i].oldPos[1]) + 10;            
-        }
-        if (z < 0) {
-            particles[i].curPos[2] = -particles[i].curPos[2];
-            particles[i].oldPos[2] = -particles[i].oldPos[2];
-        }
-        if (z > 10) {
-           particles[i].curPos[2] = 10 - (particles[i].curPos[2] - 10);
-            particles[i].oldPos[2] = (10 - particles[i].oldPos[2]) + 10;             
-        }
+        part.constraints(Vector3d(0,0,0), Vector3d(10,10,10), particles);
+        particles[i] = part;
     }
 }
