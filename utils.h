bca1bd3cc8af3a350246fcd81af9aeaa62168148
James Sha <jamessha78@berkeley.edu>
2013-11-30 18:01:22 -0800

    forgot to add utils.h

--- utils.h
+++ utils.h
@@ -1,5 +1,7 @@
 #include <Eigen/Core>
 
+using namespace Eigen;
+
 Matrix4f Rodrigues(double x, double y, double z, double theta_deg){
 	Matrix4f matrix = Matrix4f::Identity();
 	double theta = theta_deg/180*M_PI;
@@ -60,4 +62,58 @@ Vector3d homogeneous_to_euc_dir(Vector4d& v){
 	return out;
 }
 
+// The following code from https://github.com/curran/renderCyliner
+void renderCylinder(float x1, float y1, float z1, float x2,float y2, float z2, float radius,int subdivisions,GLUquadricObj *quadric)
+{
+  float vx = x2-x1;
+  float vy = y2-y1;
+  float vz = z2-z1;
+  float v = sqrt( vx*vx + vy*vy + vz*vz );
+  float ax;
 
+  if (fabs(vz) < 1.0e-3) {
+    ax = 57.2957795*acos( vx/v ); // rotation angle in x-y plane
+    if ( vy <= 0.0 )
+      ax = -ax;
+  }
+  else {
+    ax = 57.2957795*acos( vz/v ); // rotation angle
+    if ( vz <= 0.0 )
+      ax = -ax;
+  }
+  
+  float rx = -vy*vz;
+  float ry = vx*vz;
+  
+  glPushMatrix();
+  //draw the cylinder body
+  glTranslatef( x1,y1,z1 );
+  if (fabs(vz) < 1.0e-3) {
+    glRotated(90.0, 0, 1, 0.0); // Rotate & align with x axis
+    glRotated(ax, -1.0, 0.0, 0.0); // Rotate to point 2 in x-y plane
+  }
+  else {
+    glRotated(ax, rx, ry, 0.0); // Rotate about rotation vector
+  }
+  gluQuadricOrientation(quadric,GLU_OUTSIDE);
+  gluCylinder(quadric, radius, radius, v, subdivisions, 1);
+
+  //draw the first cap
+  gluQuadricOrientation(quadric,GLU_INSIDE);
+  gluDisk( quadric, 0.0, radius, subdivisions, 1);
+  glTranslatef( 0,0,v );
+
+  //draw the second cap
+  gluQuadricOrientation(quadric,GLU_OUTSIDE);
+  gluDisk( quadric, 0.0, radius, subdivisions, 1);
+  glPopMatrix();
+}
+
+void renderCylinder_convenient(float x1, float y1, float z1, float x2,float y2, float z2, float radius,int subdivisions)
+{
+  //the same quadric can be re-used for drawing many cylinders
+  GLUquadricObj *quadric=gluNewQuadric();
+  gluQuadricNormals(quadric, GLU_SMOOTH);
+  renderCylinder(x1,y1,z1,x2,y2,z2,radius,subdivisions,quadric);
+  gluDeleteQuadric(quadric);
+}
